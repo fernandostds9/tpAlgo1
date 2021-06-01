6b41c95d793a1bf7ad6374cc7d7c7a2b83090b1d
fernandostds9 <jnrmn95@gmail.com>
2021-06-01 03:31:33 -0300

    Create CTestTestfile.cmake

---
+++ src/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/iherrera/Documents/UBA/2018_2C/algo1_taller/TPI3/enunciadoTPI_v05.11/src/lib/googletest-master
+# Build directory: /home/iherrera/Documents/UBA/2018_2C/algo1_taller/TPI3/enunciadoTPI_v05.11/src/cmake-build-debug/lib/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
